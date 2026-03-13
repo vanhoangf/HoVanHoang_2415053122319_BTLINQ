f6d36f20882808fe8cf5901161974dd21241fb4d
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-13 23:24:54 +0700

    BTLINQ Bai6

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Ho Van Hoang\nMSV: 2415053122319\nLop: 225LTC01\n");
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var sc = numbers.Count(x => x % 2 == 0);
+            Console.Write("So phan tu chan la: {0}", sc);
+            Console.ReadKey();
+        }
+    }
+}
