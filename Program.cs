dce496424876118b77fd3a54be6f10690530e821
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-13 23:36:35 +0700

    BTLINQ Bai10

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Ho Van Hoang\nMSV: 2415053122319\nLop: 225LTC01\n");
+            List<Student> students = new List<Student>()
+            { new Student { Id = 1, Name = "An", Score = 8 },
+              new Student { Id = 2, Name = "Binh", Score = 6 },
+              new Student { Id = 3, Name = "Chi", Score = 9 },
+              new Student { Id = 4, Name = "Dung", Score = 7 } };
+
+            var sx = students.OrderByDescending(x => x.Score);
+
+            foreach (var student in sx)
+                Console.WriteLine(student);
+            Console.ReadKey();
+        }
+    }
+}
