e7a5a174932ce43841cae314aec94db2f0046f07
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-13 23:18:50 +0700

    BTLINQ Bai4

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Ho Van Hoang\nMSV: 2415053122319\nLop: 225LTC01\n");
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            var sx = numbers.OrderByDescending(x => x);
+            Console.Write("Danh sach sau khi sap xep: ");
+            foreach (var x in sx)
+                Console.Write("{0} ", x);
+            Console.ReadKey();
+        }
+    }
+}
