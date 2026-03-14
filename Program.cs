c2177b3300420bdb79fe795ed1df060f304cbcba
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-14 16:35:38 +0700

    BTLINQ Bai15

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Ho Van Hoang\nMSV: 2415053122319\nLop: 225LTC01\n");
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5};
+
+            numbers = numbers.Distinct().ToList();
+            foreach (int num in numbers)
+                Console.Write(" {0} ", num);
+
+            Console.ReadKey();
+        }
+    }
+}
