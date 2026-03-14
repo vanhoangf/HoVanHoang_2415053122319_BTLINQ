741997678efdb17d01fc5800382e6926c9cad1ce
Hồ Hoàng <hovanhoang959@gmail.com>
2026-03-14 16:44:00 +0700

    BTLINQ Bai16

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bai16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Ho Van Hoang\nMSV: 2415053122319\nLop: 225LTC01\n");
+            List<int> numbers = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+
+            var tong = numbers.Sum();
+            Console.WriteLine("Tong day la: {0}", tong);
+
+            Console.ReadKey();
+        }
+    }
+}
